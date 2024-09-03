25cda7fb42a0852be92b9ece9b41f33049aba38c
Tilak Singh <tsingh99991@gmail.com>
2024-09-03 09:13:14 +0530

    data added and card added

--- src/app/(app)/jobs/page.tsx
+++ src/app/(app)/jobs/page.tsx
@@ -1,19 +1,31 @@
-"use client"
-import JobCard from '@/components/JobCard'
-import React from 'react'
+"use client";
+import JobCard from "@/components/JobCard";
+import React from "react";
+import { jobData } from "@/data";
 
 export default function page() {
   return (
-    <div className='text-white h-full px-3'>
+    <div className="text-white h-full px-3">
       <div className="flex justify-between py-8 px-5 items-center">
-        <h1 className='lg:text-4xl font-bold '>Discover Your Next Remote Opportunity</h1>
+        <h1 className="lg:text-4xl font-bold ">
+          Discover Your Next Remote Opportunity
+        </h1>
         <div className="text-sm text-gray-400">203 jobs found</div>
       </div>
       <div className="grid grid-cols-3">
-        <JobCard />
-        <JobCard />
-        <JobCard />
+        {jobData.map((job) => (
+          <JobCard
+            key={job.key}
+            time_stamp={job.time_stamp}
+            company_name={job.company_name}
+            job_description={job.job_description}
+            experience_level={job.experience_level}
+            job_title={job.job_title}
+            job_type={job.job_type}
+            location={job.location}
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
--- src/components/JobCard.tsx
+++ src/components/JobCard.tsx
@@ -1,43 +1,68 @@
 "use client";
 import { Bookmark, Building2, MapPin, MoveRight } from "lucide-react";
 import React from "react";
+interface JobCardData {
+  company_name: string;
+  job_title: string;
+  job_type: string;
+  experience_level: string;
+  job_description: string;
+  time_stamp: string;
+  location: string;
+}
 
-function JobCard() {
+function JobCard({
+  company_name,
+  job_title,
+  job_type,
+  job_description,
+  location,
+  experience_level,
+  time_stamp,
+}: JobCardData) {
   return (
     <div className="p-3 mb-6">
       <div className=" bg-black shadow-lg p-6 flex flex-col gap-3 rounded-xl">
-      <div className="flex justify-between items-center">
-        <p className="font-semibold text-lg text-ellipsis flex gap-1 items-center"><Building2 size={16} strokeWidth={1.25} absoluteStrokeWidth />
-      Company</p>
+        <div className="flex justify-between items-center">
+          <p className="font-semibold text-lg text-ellipsis flex gap-1 items-center">
+            <Building2 size={16} strokeWidth={1.25} absoluteStrokeWidth />
+            {company_name}
+          </p>
 
-        <div className=" flex gap-1 text-sm text-gray-300 hover:text-white items-center  transition-all duration-500 ease-in-out">
-          <span>Save</span> <Bookmark size={"16px"}/>
+          <div className=" flex gap-1 text-sm text-gray-300 hover:text-white items-center  transition-all duration-500 ease-in-out">
+            <span>Save</span> <Bookmark size={"16px"} />
+          </div>
         </div>
-        
-      </div>
-      <div className="flex justify-start flex-col">
-        <h2 className="lg:text-4xl font-bold ">Entry Backend Engineer</h2>
-        <span className=" mt-1 p-1 text-xs rounded-lg bg-blue-600 max-w-max hover:bg-transparent transition-all duration-500 ease-in-out ">Full-time</span>
-      </div>
-      <div className="text-sm text-gray-200 flex items-center "><MapPin size={16} strokeWidth={1.5} absoluteStrokeWidth /> India, New Delhi</div>
-      <div className="text-sm ">
-        <div className="font-normal m-1 text-gray-200">Experience Level: Executive</div>
-        <div className="">
-          <p className="text-clip text-xs text-gray-400">
-            <span className=" font-semibold text-sm">Job Description :</span>
-            Lorem ipsum dolor sit ame Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, odio?          </p>
+        <div className="flex justify-start flex-col">
+          <h2 className="lg:text-4xl font-bold ">{job_title}</h2>
+          <span className=" mt-1 p-1 text-xs rounded-lg bg-blue-600 max-w-max hover:bg-transparent transition-all duration-500 ease-in-out ">
+            {time_stamp}
+          </span>
         </div>
-      </div>
-      
-      <div className="flex justify-between items-center h-9">
-      <span className="text-sm font-semibold ">15 hours ago</span>
+        <div className="text-sm text-gray-200 flex items-center ">
+          <MapPin size={16} strokeWidth={1.5} absoluteStrokeWidth /> {location}
+        </div>
+        <div className="text-sm ">
+          <div className="font-normal m-1 text-gray-200">
+            {experience_level}
+          </div>
+          <div className="">
+            <p className="text-clip text-xs text-gray-400">
+              <span className=" font-semibold text-sm">Job Description :</span>
+              {job_description}{" "}
+            </p>
+          </div>
+        </div>
 
-        <button className="bg-green-500 text-sm gap-2 py-1.5 px-2 rounded-3xl flex items-center hover:bg-green-600 hover:text-[16px]  transition-all duration-500 ease-in-out">
-          Apply Now <MoveRight size={16} strokeWidth={2} absoluteStrokeWidth />
-        </button>
-  
-    </div>
-    </div>
+        <div className="flex justify-between items-center h-9">
+          <span className="text-sm font-semibold ">{time_stamp}</span>
+
+          <button className="bg-green-500 text-sm gap-2 py-1.5 px-2 rounded-3xl flex items-center hover:bg-green-600 hover:text-[16px]  transition-all duration-500 ease-in-out">
+            Apply Now{" "}
+            <MoveRight size={16} strokeWidth={2} absoluteStrokeWidth />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
